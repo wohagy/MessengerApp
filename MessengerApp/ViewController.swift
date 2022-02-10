d3ae69275eb56d2cd32407deadc12eac3432366d
Emelyanov Roman <wohagy@gmail.com>
2022-02-10 16:30:36 +0300

    added canvas previewer

--- MessengerApp/ViewController.swift
+++ MessengerApp/ViewController.swift
@@ -17,3 +17,24 @@ class ViewController: UIViewController {
 
 }
 
+
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerProvider.ContainerView>) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: ViewControllerProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewControllerProvider.ContainerView>) {
+            
+        }
+    }
+}
